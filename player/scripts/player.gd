16bd90dcdcf26429cfc4f93161d2427d02361cc5
Dekara VanHoc <vanhocgames@gmail.com>
2023-11-11 18:58:35 +0100

    add money collect, upgrade function and win stuff

--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -3,12 +3,15 @@ extends Area2D
 
 enum Controller {Controller1, Controller2}
 
-var health: int = 100
+var health: int = 10000
 var money: int = 0
+
 var dmg = 0
 var def: int = 1
 
 @export var controller: Controller
+@export var enemy_player: Player
+@export var win_message_label: Label
 
 var input_device: int = 0
 signal health_depleted
@@ -18,6 +21,16 @@ var current_health: int:
 		current_health = min(health, max(0, value))
 		if current_health == 0:
 			health_depleted.emit()
+			win_message_label.show()
+			if(controller == Controller.Controller1):
+				win_message_label.text = "Player 2 WON!!!!"
+			else:
+				win_message_label.text = "Player 1 WON!!!!"
+
+			for unit in get_tree().get_nodes_in_group("Units"):
+				unit.behavior.change_state(StateDie.new())
+			await get_tree().create_timer(3).timeout
+			get_tree().reload_current_scene()
 
 func _ready():
 	current_health = health
@@ -29,9 +42,6 @@ func _ready():
 		collision_layer = 2
 		input_device = 1
 
-func _process(delta):
-	pass
-	$hud.update_money(money)
 	
 func _input(event):
 	if(event.is_action_pressed("debug_player2_create") && controller == Controller.Controller2):
@@ -47,14 +57,29 @@ func _input(event):
 func create_unit():
 	if(controller == Controller.Controller1):
 		var current_unit = preload("res://unit/unit.tscn").instantiate()
-		current_unit.spawn(self.get_parent(), Unit.PLAYER.Player1, self.global_position + Vector2(20,0))
+		current_unit.spawn(self.get_parent(), Unit.PLAYER.Player1, self.global_position + Vector2(20,0), enemy_player.collect_gold)
 	if(controller == Controller.Controller2):
 		var current_unit = preload("res://unit/unit_boxer.tscn").instantiate()
-		current_unit.spawn(self.get_parent(), Unit.PLAYER.Player2, self.global_position - Vector2(20,0))
+		current_unit.spawn(self.get_parent(), Unit.PLAYER.Player2, self.global_position - Vector2(20,0), enemy_player.collect_gold)
 	
 func hit(amount: int):
 	current_health -= amount
 	$hud.update_health(100.0 * current_health / health)
+		
+
+
+func collect_gold(amount: int):
+	money += amount
+	$hud.update_money(money)
+
+func pay_gold(amount: int):
+	if not has_enough_gold(amount):
+		return
+	money -= amount
+	$hud.update_money(money)
+
+func has_enough_gold(amount: int):
+	return money >= amount
 
 func open_menu():
 	print("menu")
