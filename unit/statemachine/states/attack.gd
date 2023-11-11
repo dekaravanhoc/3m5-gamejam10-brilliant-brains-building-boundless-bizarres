c4019f6610cbaf607dca4f1753d5c5b6d15b7dc4
Felix Schulze <felix.schulze@3m5.de>
2023-11-11 13:33:52 +0100

    Add attack and die animations

---
+++ unit/statemachine/states/attack.gd
@@ -0,0 +1,10 @@
+class_name StateAttack
+extends State
+
+
+func start():
+	behavior.unit.animated_sprite.play("attack")
+
+
+func run(delta: float):
+	pass
---
+++ unit/statemachine/states/die.gd
@@ -0,0 +1,10 @@
+class_name StateDie
+extends State
+
+
+func start():
+	behavior.unit.animated_sprite.play("die")
+
+
+func run(delta: float):
+	pass
